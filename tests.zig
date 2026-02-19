a5c1ffc5e0ec511a5fc4daf1318959a8fe2cf488
Richard Remer <rich.remer@gmail.com>
2026-02-19 09:44:05 -0800

    import testing into module namespace

--- tests.zig
+++ tests.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
+const testing = std.testing;
 
 // using testing allocator for these tests; Zig provides various allocators for
-// different use cases; the testing allocator reports memory leaks
-const allocator = std.testing.allocator;
+// different use cases; the testing allocator reports memory leaks and other
+// misuses, such as double free.
+const allocator = testing.allocator;
 
 const TestError = error{
     AnError,
@@ -16,34 +18,34 @@ fn raiseAnError() TestError!void {
 // TODO: demonstrate container variable order flexibility
 
 test "literals" {
-    try std.testing.expectEqual(bool, @TypeOf(true));
-    try std.testing.expectEqual(comptime_int, @TypeOf(42));
-    try std.testing.expectEqual(comptime_float, @TypeOf(3.14159));
-    try std.testing.expectEqual(comptime_int, @TypeOf('a'));
-    try std.testing.expectEqual(*const [3:0]u8, @TypeOf("foo"));
+    try testing.expectEqual(bool, @TypeOf(true));
+    try testing.expectEqual(comptime_int, @TypeOf(42));
+    try testing.expectEqual(comptime_float, @TypeOf(3.14159));
+    try testing.expectEqual(comptime_int, @TypeOf('a'));
+    try testing.expectEqual(*const [3:0]u8, @TypeOf("foo"));
 
-    try std.testing.expectEqual(255, 0xff); // hex lowercase
-    try std.testing.expectEqual(255, 0xFF); // hex uppercase
-    try std.testing.expectEqual(255, 0o377); // octal
-    try std.testing.expectEqual(255, 0b11111111); // binary
-    try std.testing.expectEqual(1000, 1_000); // underscore separator
+    try testing.expectEqual(255, 0xff); // hex lowercase
+    try testing.expectEqual(255, 0xFF); // hex uppercase
+    try testing.expectEqual(255, 0o377); // octal
+    try testing.expectEqual(255, 0b11111111); // binary
+    try testing.expectEqual(1000, 1_000); // underscore separator
 
     // TODO: 'type' type
 }
 
 test "escapes" {
-    try std.testing.expectEqual(9, '\t');
-    try std.testing.expectEqual(10, '\n');
-    try std.testing.expectEqual(13, '\r');
-    try std.testing.expectEqual(39, '\'');
-    try std.testing.expectEqual(34, '\"');
-    try std.testing.expectEqual(92, '\\');
-    try std.testing.expectEqual(96, '\x60'); // hex 60 === dec 96
-    try std.testing.expectEqual(584, '\u{248}'); // UTF-8 encoded
+    try testing.expectEqual(9, '\t');
+    try testing.expectEqual(10, '\n');
+    try testing.expectEqual(13, '\r');
+    try testing.expectEqual(39, '\'');
+    try testing.expectEqual(34, '\"');
+    try testing.expectEqual(92, '\\');
+    try testing.expectEqual(96, '\x60'); // hex 60 === dec 96
+    try testing.expectEqual(584, '\u{248}'); // UTF-8 encoded
 
     // raises compile-time error: unicode escape does not correspond to a valid
     // codepoint
-    // try std.testing.expectEqual(1114112, '\u{110000}');
+    // try testing.expectEqual(1114112, '\u{110000}');
 }
 
 test "constants" {
@@ -54,7 +56,7 @@ test "constants" {
     // raises compile-time error: cannot assign to constant
     // immutable = 42;
 
-    try std.testing.expectEqual(42, immutable);
+    try testing.expectEqual(42, immutable);
 }
 
 test "variables" {
@@ -67,7 +69,7 @@ test "variables" {
     // '&' operator avoids compile-time error: local variable is never mutated
     _ = &addressed;
 
-    try std.testing.expectEqual(23, mutable);
+    try testing.expectEqual(23, mutable);
 }
 
 // TODO: test undefined values
@@ -83,12 +85,12 @@ test "optional values" {
     // raises compile-time error: expected type 'u8', found '?u8'
     // const required: u8 = optional_with_value;
 
-    try std.testing.expect(optional_without_value == null);
-    try std.testing.expect(optional_without_value != 0);
-    try std.testing.expectEqual(optional_with_value orelse fallback, value);
-    try std.testing.expectEqual(optional_without_value orelse fallback, fallback);
-    try std.testing.expect(@typeInfo(RequiredType) != .optional);
-    try std.testing.expect(@typeInfo(OptionalType) == .optional);
+    try testing.expect(optional_without_value == null);
+    try testing.expect(optional_without_value != 0);
+    try testing.expectEqual(optional_with_value orelse fallback, value);
+    try testing.expectEqual(optional_without_value orelse fallback, fallback);
+    try testing.expect(@typeInfo(RequiredType) != .optional);
+    try testing.expect(@typeInfo(OptionalType) == .optional);
 }
 
 test "integer types" {
@@ -106,11 +108,11 @@ test "integer types" {
     // raises compile-time error: type 'u1' cannot represent integer value '2'
     // bit = 2;
 
-    try std.testing.expectEqual(1, bit);
-    try std.testing.expectEqual(7, std.math.maxInt(@TypeOf(octal)));
-    try std.testing.expectEqual(0, std.math.minInt(@TypeOf(octal)));
-    try std.testing.expectEqual(1, std.math.maxInt(@TypeOf(signed)));
-    try std.testing.expectEqual(-2, std.math.minInt(@TypeOf(signed)));
+    try testing.expectEqual(1, bit);
+    try testing.expectEqual(7, std.math.maxInt(@TypeOf(octal)));
+    try testing.expectEqual(0, std.math.minInt(@TypeOf(octal)));
+    try testing.expectEqual(1, std.math.maxInt(@TypeOf(signed)));
+    try testing.expectEqual(-2, std.math.minInt(@TypeOf(signed)));
 }
 
 test "float types" {
@@ -122,34 +124,34 @@ test "float types" {
     const extended: c_longdouble = double;
     const quadruple: f128 = extended;
 
-    try std.testing.expectEqual(3.140625, quadruple);
+    try testing.expectEqual(3.140625, quadruple);
 
-    try std.testing.expectEqual(5, std.math.floatExponentBits(f16));
-    try std.testing.expectEqual(8, std.math.floatExponentBits(f32));
-    try std.testing.expectEqual(11, std.math.floatExponentBits(f64));
-    try std.testing.expectEqual(15, std.math.floatExponentBits(c_longdouble));
-    try std.testing.expectEqual(15, std.math.floatExponentBits(f128));
+    try testing.expectEqual(5, std.math.floatExponentBits(f16));
+    try testing.expectEqual(8, std.math.floatExponentBits(f32));
+    try testing.expectEqual(11, std.math.floatExponentBits(f64));
+    try testing.expectEqual(15, std.math.floatExponentBits(c_longdouble));
+    try testing.expectEqual(15, std.math.floatExponentBits(f128));
 
-    try std.testing.expectEqual(10, std.math.floatMantissaBits(f16));
-    try std.testing.expectEqual(23, std.math.floatMantissaBits(f32));
-    try std.testing.expectEqual(52, std.math.floatMantissaBits(f64));
-    try std.testing.expectEqual(64, std.math.floatMantissaBits(c_longdouble));
-    try std.testing.expectEqual(112, std.math.floatMantissaBits(f128));
+    try testing.expectEqual(10, std.math.floatMantissaBits(f16));
+    try testing.expectEqual(23, std.math.floatMantissaBits(f32));
+    try testing.expectEqual(52, std.math.floatMantissaBits(f64));
+    try testing.expectEqual(64, std.math.floatMantissaBits(c_longdouble));
+    try testing.expectEqual(112, std.math.floatMantissaBits(f128));
 
-    try std.testing.expectEqual(10, std.math.floatFractionalBits(f16));
-    try std.testing.expectEqual(23, std.math.floatFractionalBits(f32));
-    try std.testing.expectEqual(52, std.math.floatFractionalBits(f64));
-    try std.testing.expectEqual(63, std.math.floatFractionalBits(c_longdouble));
-    try std.testing.expectEqual(112, std.math.floatFractionalBits(f128));
+    try testing.expectEqual(10, std.math.floatFractionalBits(f16));
+    try testing.expectEqual(23, std.math.floatFractionalBits(f32));
+    try testing.expectEqual(52, std.math.floatFractionalBits(f64));
+    try testing.expectEqual(63, std.math.floatFractionalBits(c_longdouble));
+    try testing.expectEqual(112, std.math.floatFractionalBits(f128));
 
-    try std.testing.expectEqual(1e-45, std.math.floatTrueMin(f32));
-    try std.testing.expectEqual(1.1754944e-38, std.math.floatMin(f32));
-    try std.testing.expectEqual(3.4028235e38, std.math.floatMax(f32));
-    try std.testing.expectEqual(1.1920929e-7, std.math.floatEps(f32));
-    try std.testing.expectEqual(1.1920929e-7, std.math.floatEpsAt(f32, 1.0));
+    try testing.expectEqual(1e-45, std.math.floatTrueMin(f32));
+    try testing.expectEqual(1.1754944e-38, std.math.floatMin(f32));
+    try testing.expectEqual(3.4028235e38, std.math.floatMax(f32));
+    try testing.expectEqual(1.1920929e-7, std.math.floatEps(f32));
+    try testing.expectEqual(1.1920929e-7, std.math.floatEpsAt(f32, 1.0));
 
-    try std.testing.expectEqual(std.math.inf(f32), std.math.inf(f16));
-    try std.testing.expect(std.math.nan(f32) != std.math.nan(f32));
+    try testing.expectEqual(std.math.inf(f32), std.math.inf(f16));
+    try testing.expect(std.math.nan(f32) != std.math.nan(f32));
 
     // raises compile-time error: division by zero here causes undefined behavior
     // const div0 = 1.0 / 0.0;
@@ -205,8 +207,8 @@ test "static container variables" {
     // q.v. fn nextStatic() u32
     // function declares a const struct, which acts as a static local variable
 
-    try std.testing.expectEqual(1, nextStatic());
-    try std.testing.expectEqual(2, nextStatic());
+    try testing.expectEqual(1, nextStatic());
+    try testing.expectEqual(2, nextStatic());
 }
 // used in the previous test to demonstrate thread local storage via container
 fn nextStatic() u32 {
@@ -218,13 +220,13 @@ fn nextStatic() u32 {
 }
 
 test "error namespace" {
-    try std.testing.expect(error.AnError == TestError.AnError);
-    try std.testing.expect(error.AnError != TestError.AnotherError);
+    try testing.expect(error.AnError == TestError.AnError);
+    try testing.expect(error.AnError != TestError.AnotherError);
 }
 
 test "returning error" {
     const err = raiseAnError();
-    try std.testing.expectError(TestError.AnError, err);
+    try testing.expectError(TestError.AnError, err);
 }
 
 test "array sentinels" {
@@ -235,16 +237,16 @@ test "array sentinels" {
     const with_sentinel = [_:0]u8{ 'f', 'o', 'o' };
     const without_sentinel = [_]u8{ 'f', 'o', 'o' };
 
-    try std.testing.expectEqual(all_sentinels.len, 3);
-    try std.testing.expectEqual(with_sentinel.len, 3);
-    try std.testing.expectEqual(without_sentinel.len, 3);
-    try std.testing.expectEqual(with_sentinel[3], 0);
-    try std.testing.expectEqual(all_sentinels[3], 0);
+    try testing.expectEqual(all_sentinels.len, 3);
+    try testing.expectEqual(with_sentinel.len, 3);
+    try testing.expectEqual(without_sentinel.len, 3);
+    try testing.expectEqual(with_sentinel[3], 0);
+    try testing.expectEqual(all_sentinels[3], 0);
 
     // raises compile-time error: index 3 outside array of length 3
     // _ = without_sentinel[3];
 
-    try std.testing.expect(std.mem.eql(u8, &with_sentinel, &without_sentinel));
+    try testing.expect(std.mem.eql(u8, &with_sentinel, &without_sentinel));
 }
 
 test "multiline string literals" {
@@ -256,17 +258,17 @@ test "multiline string literals" {
         \\2
     ;
 
-    try std.testing.expectEqual(7, string.len);
-    try std.testing.expectEqual("foo\\x23", string);
-    try std.testing.expectEqual(3, string_with_newlines.len);
-    try std.testing.expectEqual('\n', string_with_newlines[1]);
+    try testing.expectEqual(7, string.len);
+    try testing.expectEqual("foo\\x23", string);
+    try testing.expectEqual(3, string_with_newlines.len);
+    try testing.expectEqual('\n', string_with_newlines[1]);
 }
 
 test "memory allocation" {
     var list = std.array_list.Managed(u8).init(allocator);
 
     try list.append(23);
-    try std.testing.expectEqual(1, list.items.len);
+    try testing.expectEqual(1, list.items.len);
 
     // avoids run-time error: memory address 0x... leaked:
     list.deinit();
@@ -277,7 +279,7 @@ test "defer" {
     defer list.deinit(); // more sensible place to put cleanup using 'defer'
 
     try list.append(23);
-    try std.testing.expectEqual(1, list.items.len);
+    try testing.expectEqual(1, list.items.len);
 }
 
 test "thread local variables" {
@@ -293,9 +295,9 @@ test "thread local variables" {
 // these are used in the previous test to demonstrate thread local variables
 threadlocal var thread_value: u32 = 0;
 fn testThreadValue() !void {
-    try std.testing.expectEqual(0, thread_value);
+    try testing.expectEqual(0, thread_value);
     thread_value += 1;
-    try std.testing.expectEqual(1, thread_value);
+    try testing.expectEqual(1, thread_value);
 }
 
 // TODO: addition, including wrapping (+/-%), saturating (+/-|), and chaos (+/-)
@@ -310,7 +312,7 @@ test "alloc/free" {
     // thread panic: Invalid free
     // allocator.destroy(@as(*u8, @ptrCast(bytes.ptr)));
 
-    try std.testing.expectEqual([]u8, @TypeOf(bytes));
+    try testing.expectEqual([]u8, @TypeOf(bytes));
 }
 
 test "create/destroy" {
@@ -321,7 +323,7 @@ test "create/destroy" {
     // raises error: Expected pointer, slice, array, or vector type, found '...*Type'
     // allocator.free(object);
 
-    try std.testing.expectEqual(*Type, @TypeOf(object));
+    try testing.expectEqual(*Type, @TypeOf(object));
 }
 
 test "alloc/destroy" {
@@ -334,7 +336,7 @@ test "alloc/destroy" {
     // Segmentation fault at address 0x...
     // allocator.destroy(@as(*usize, @ptrCast(@alignCast(bytes.ptr))));
 
-    try std.testing.expectEqual([]u8, @TypeOf(bytes));
+    try testing.expectEqual([]u8, @TypeOf(bytes));
 }
 
 test "create/free" {
@@ -348,5 +350,5 @@ test "create/free" {
     // warning: Allocation size 8 bytes does not match free size 1. Allocation:...
     // allocator.free(@as(*align(@sizeOf(Type)) [1]u8, @ptrCast(object)));
 
-    try std.testing.expectEqual(*Type, @TypeOf(object));
+    try testing.expectEqual(*Type, @TypeOf(object));
 }
